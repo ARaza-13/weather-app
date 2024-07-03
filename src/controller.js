dff53ddf17320e75d8b8ad327bf1aecb5302e54c
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-07-02 21:54:46 -0400

    Created functions to extract the weather API data and display it on the screen. Applied styling to the weather card

--- src/controller.js
+++ src/controller.js
@@ -11,9 +11,38 @@ export default class Controller {
 
     try {
       const weatherData = await Data.fetchWeather(location);
-      console.log(weatherData);
+
+      if (!weatherData) {
+        return;
+      }
+
+      Controller.displayWeather(weatherData);
     } catch (error) {
       console.log(`Error: ${error}`);
     }
+  }
+
+  static displayWeather(weatherData) {
+    console.log(weatherData);
+    const weatherObj = Data.extractWeatherData(weatherData);
+    console.log(weatherObj);
+
+    const location = document.getElementById("location");
+    const weatherIcon = document.getElementById("weather-icon");
+    const description = document.getElementById("description");
+    const temperature = document.getElementById("temperature");
+    const feelTemperature = document.getElementById("feels-like");
+    const wind = document.getElementById("wind");
+    const humidity = document.getElementById("humidity");
+    const cloudiness = document.getElementById("cloudiness");
+
+    location.textContent = weatherObj.location;
+    weatherIcon.src = weatherObj.icon;
+    description.textContent = weatherObj.weatherDescription;
+    temperature.textContent = weatherObj.temperatureF;
+    feelTemperature.textContent = weatherObj.feelTemperatureF;
+    wind.textContent = weatherObj.windMph;
+    humidity.textContent = weatherObj.humidity;
+    cloudiness.textContent = weatherObj.cloud;
   }
 }
--- src/data.js
+++ src/data.js
@@ -20,4 +20,34 @@ export default class Data {
       return null;
     }
   }
+
+  static extractWeatherData(weatherData) {
+    const city = weatherData.location.name;
+    const country = weatherData.location.country;
+    const weatherIcon = weatherData.current.condition.icon;
+    const temperatureF = weatherData.current.temp_f;
+    const temperatureC = weatherData.current.temp_c;
+    const feelTemperatureF = weatherData.current.feelslike_f;
+    const feelTemperatureC = weatherData.current.feelslike_c;
+    const weatherDescription = weatherData.current.condition.text;
+    const windMph = weatherData.current.wind_mph;
+    const windKph = weatherData.current.wind_kph;
+    const humidity = weatherData.current.humidity;
+    const cloud = weatherData.current.cloud;
+
+    const data = {};
+    data.location = `${city}, ${country}`;
+    data.icon = weatherIcon;
+    data.temperatureF = `${temperatureF}°F`;
+    data.temperatureC = `${temperatureC}°C`;
+    data.feelTemperatureF = `Feels like ${feelTemperatureF}°F`;
+    data.feelTemperatureC = `Feels like ${feelTemperatureC}°C`;
+    data.weatherDescription = weatherDescription;
+    data.windMph = `${windMph}mph`;
+    data.windKph = `${windKph}kph`;
+    data.humidity = `${humidity}%`;
+    data.cloud = `${cloud}%`;
+
+    return data;
+  }
 }
